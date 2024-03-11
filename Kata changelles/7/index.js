9e46a0f25dd9a38033c7226e8af832eef3fba97d
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-11 09:25:14 -0700

    feat: :technologist: done kata challenge 7

---
+++ Kata changelles/7/index.js
@@ -0,0 +1,10 @@
+// Escribe tu código aquí
+
+const letter = (l) => {
+
+  if (l == 'a' || l == 'e' || l == 'i' || l == 'o' || l == 'u') {
+    return "Vocal"
+  } else {
+    return "Consonante"
+  }
+};
