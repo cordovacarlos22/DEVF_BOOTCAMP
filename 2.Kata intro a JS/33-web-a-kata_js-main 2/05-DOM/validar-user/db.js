8cd400c2ef672187f2475f80f4a716bbeeeaab7e
carloscordova.dev <cordovamanuel22@icloud.com>
2024-04-01 09:05:31 -0700

    add class code

---
+++ 2.Kata intro a JS/33-web-a-kata_js-main 2/05-DOM/validar-user/db.js
@@ -0,0 +1,7 @@
+let cuentas = [
+    { nombre: 'mali', pass: 1234, saldo: 200 },
+    { nombre: 'gera', pass: 5678, saldo: 290 },
+    { nombre: 'maui', pass: 9012, saldo: 67 },
+    { nombre: 'pepe', pass: 1234, saldo: 21 },
+    { nombre: 'carlos', pass: 5678, saldo: 670 }
+]; 
