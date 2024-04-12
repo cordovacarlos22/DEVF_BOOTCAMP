f896331d9b33eda21ea8956729b5bdbe23a624fe
carloscordova.dev <cordovamanuel22@icloud.com>
2024-04-11 20:21:45 -0700

    update to ejemplo2.js

--- 3.Kata Computer Science/semana 1 /clase 4-11-24/ejemplo2.js
+++ 3.Kata Computer Science/semana 1 /clase 4-11-24/ejemplo2.js
@@ -1,25 +1,30 @@
+//Javascript no cuenta con el control del alcance de sus variable
+//Sin embargo, existen convenciones que nos pueden ayudar a indicar cuando
+//un elemento debe ser privado, es decir, que sólo lo manipularemos pr medio
+//de métodos dentro de la clase
 
 
-// js no cuenta con el control de alcance de su variables 
-// sin imbargo existen convenciones que nos pueden ayudar a indicar  cuando
-// un elemento debe ser privado , es decir, que solo lo manipularemos
-// de metodos dentro de la clase
-class Contador{
+class Contador {
   constructor(valor) {
-    
-    //utilizar "_" para definir que un atributo o metodo lo utilisaremos privado 
-    // es decir,que solo lo manipularemos dentro de la clase
-    this._valor = valor;
+    //Utilizar "_" para definir que un atributo o método lo usaremos como privado,
+    //es decir, que sólo lo manipularemos dentro de la clase
+    this._valor = valor
+
   }
+
   sumar() {
-    this._valor = this._valor + 1;
+    this._valor = this._valor + 1
   }
 
   obtenerValor() {
-    return this._valor;
+    return this._valor
   }
 }
 
-let contador = new Contador(0);  
-contador.sumar();
+let contador = new Contador(0)
+
+contador.sumar()
+contador.sumar()
+//contador._valor = 20 <- sintácticamente es correcto, sin embargo, por especificación esto no debería hacerse
+
 console.log(contador.obtenerValor());
