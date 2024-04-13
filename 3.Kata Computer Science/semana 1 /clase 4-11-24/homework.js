1401c8b1fcb2401058ecb12df5543c7f8c7d0e3f
carloscordova.dev <cordovamanuel22@icloud.com>
2024-04-13 09:39:17 -0700

    done with homewo

--- 3.Kata Computer Science/semana 1 /clase 4-11-24/homework.js
+++ 3.Kata Computer Science/semana 1 /clase 4-11-24/homework.js
@@ -1,14 +1,31 @@
 
-class CarritoDeCompras{
+class CarritoDeCompras {
   constructor() {
     this.productos = [];
   }
   agregarProducto(producto) {
+    this.producto = producto;
     this.productos.push(producto);
   }
+
   mostrarProductos() {
-    this.productos.forEach(producto => {
-      console.log(producto);
-    });
+    let total = 0
+    this.productos.map((item,) => {
+      console.log(`- ${item.nombre}: $${item.precio}`)
+    })
+    for (let i = 0; i < this.productos.length; i++) {
+      total += this.productos[i].precio
+    }
+    console.log(`Total: $${total}`)  
   }
 }
+
+let carrito = new CarritoDeCompras();
+
+carrito.agregarProducto({ 'nombre': 'hamburgueza', 'precio': 10 });
+carrito.agregarProducto({ 'nombre': 'pizza', 'precio': 30 });
+
+carrito.mostrarProductos();
+
+
+
