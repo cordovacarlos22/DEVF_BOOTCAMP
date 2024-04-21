56fb0a8880b7830f1b6502925efc121b2150579b
carloscordova.dev <cordovamanuel22@icloud.com>
2024-04-20 18:47:17 -0700

    add clase semana 2

---
+++ 3.Kata Computer Science/semana 2 /clase 4-18-24/index.js
@@ -0,0 +1,38 @@
+//  --------------------------------------------------------------------------
+
+//! pilas (stacks)
+// ------------------------------------------
+class Pila {
+  constructor() {
+    this.pila = [];
+  }
+
+  // fondo            tope
+  //  0                 5
+  // [0, 2, 9, 15, 24, -1]
+  // n = 6
+
+  // push
+  insertar(valor) {
+    this.pila.push(valor);
+  }
+  /* [0, 2, 9, 15, 24, -1] -> push(8) -> [0, 2, 9, 15, 24, -1, 8] */
+
+  // pop
+  eliminar() {
+    return this.pila.pop();
+  }
+
+  // peek
+  mostrarTope() {
+    return this.pila[this.pila.length - 1];
+  }
+
+}
+// --------------------------------------------------------------------------
+//! cola (queues)
+
+
+
+
+
