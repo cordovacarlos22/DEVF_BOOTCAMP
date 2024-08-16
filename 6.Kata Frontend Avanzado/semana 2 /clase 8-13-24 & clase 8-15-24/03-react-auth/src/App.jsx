ce13b98f5beb0f9f5cb78b95f58d3de9827dc95d
carloscordova.dev <cordovamanuel22@icloud.com>
2024-08-15 19:39:41 -0700

    add react routes

---
+++ 6.Kata Frontend Avanzado/semana 2 /clase 8-13-24 & clase 8-15-24/03-react-auth/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+import Header from './components/Header'
+import RoutesIndex from './routes/RoutesIndex'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <RoutesIndex />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
