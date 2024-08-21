d503a1a67840cd36b48a93a872d9191bc0399d5d
carloscordova.dev <cordovamanuel22@icloud.com>
2024-08-20 20:39:58 -0700

    add clase 8-20-24

--- 6.Kata Frontend Avanzado/semana 2 /clase 8-13-24 & clase 8-15-24/03-react-auth/src/App.jsx
+++ 6.Kata Frontend Avanzado/semana 2 /clase 8-13-24 & clase 8-15-24/03-react-auth/src/App.jsx
@@ -2,15 +2,18 @@ import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
 import RoutesIndex from './routes/RoutesIndex'
+import { AuthProvider } from './context/AuthContext'
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <RoutesIndex />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Header />
+          <RoutesIndex />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
--- 6.Kata Frontend Avanzado/semana 2 /clase 8-13-24 & clase 8-15-24/03-react-auth/src/pages/Login.jsx
+++ 6.Kata Frontend Avanzado/semana 2 /clase 8-13-24 & clase 8-15-24/03-react-auth/src/pages/Login.jsx
@@ -1,10 +1,39 @@
+import { useForm } from 'react-hook-form'
+import { loginUserService } from '../services/userServices'
+import { useAuthContext } from '../hooks/useAuth'
+import { useNavigate } from 'react-router-dom'
 import logo from '../assets/react.svg'
 import '../styles/form.css'
 
 const Login = () => {
+  const navigate = useNavigate()
+
+  const { login } = useAuthContext()
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await loginUserService(data)
+      if (response.status === 200) {
+        // Guardamos el token en el localStorage del navegador.
+        // Este dato permanece aún si el navegador se cierra y se vuelve a abrir.
+        login(response.data.token)
+        navigate('/dashboard')
+      }
+    } catch (error) {
+      console.error('Ocurrio un error al iniciar sesión', error.message)
+    }
+  }
+
   return (
     <main className="form-signin w-100 m-auto">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <img
           className="mb-4"
           src={logo}
@@ -19,18 +48,23 @@ const Login = () => {
             className="form-control"
             id="floatingInput"
             placeholder="name@example.com"
+            {...register('email', { required: true })}
           />
           <label htmlFor="floatingInput">Email address</label>
         </div>
+        {errors.email && <span>This field is required</span>}
+
         <div className="form-floating">
           <input
             type="password"
             className="form-control"
             id="floatingPassword"
             placeholder="Password"
+            {...register('password', { required: true })}
           />
           <label htmlFor="floatingPassword">Password</label>
         </div>
+        {errors.password && <span>This field is required</span>}
 
         <button className="btn btn-primary w-100 py-2" type="submit">
           Sign in
